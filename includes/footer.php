dc86a1216a1323512ef3062860a16f9d5aee1be1
M-BintangR <muhbintang650@gmail.com>
2025-07-23 03:41:45 +0800

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>document.write(new Date().getFullYear())</script> © Morvin.
+                        </div>
+                        <div class="col-sm-6 text-sm-end d-none d-sm-block">
+                            Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div> <!-- end main-content -->
+    </div> <!-- END layout-wrapper -->
+
+    <!-- JAVASCRIPT -->
+    <script src="../assets/libs/jquery/jquery.min.js"></script>
+    <script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/libs/metismenu/metisMenu.min.js"></script>
+    <script src="../assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="../assets/libs/node-waves/waves.min.js"></script>
+    <script src="../assets/libs/apexcharts/apexcharts.min.js"></script>
+    <script src="../assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="../assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="../assets/js/pages/dashboard.init.js"></script>
+    <script src="../assets/js/app.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?= $pageTitle ?? 'Dashboard' ?> | Morvin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link href="../assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet">
+    <link href="../assets/css/icons.min.css" rel="stylesheet">
+    <link href="../assets/css/app.min.css" id="app-style" rel="stylesheet">
+</head>
+<body>
+<div id="layout-wrapper">
