09102ba63368ff08c8a053dd86f8f74dcdb5fd6a
M-BintangR <muhbintang650@gmail.com>
2025-07-23 04:02:07 +0800

    setup template admin

---
+++ config/sidebar.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    [
+        'title'  => 'Beranda',
+        'route'  => 'index.php',
+        'icon'   => 'dripicons-home',
+    ],
+    [
+        'title'  => 'Home',
+        'route'  => '?page=home',
+        'icon'   => 'dripicons-view-thumb',
+    ],
+    [
+        'title'  => 'Profil',
+        'route'  => '?page=profil',
+        'icon'   => 'dripicons-user',
+    ],
+    [
+        'title'  => 'Gallery',
+        'route'  => '?page=galeri',
+        'icon'   => 'dripicons-photo',
+    ],
+    [
+        'title'  => 'Blog',
+        'route'  => '?page=blog',
+        'icon'   => 'dripicons-document',
+    ],
+    [
+        'title'  => 'SDM',
+        'route'  => '?page=sdm',
+        'icon'   => 'dripicons-network-2',
+    ],
+    [
+        'title'  => 'Logout',
+        'route'  => 'logout.php',
+        'icon'   => 'dripicons-exit',
+    ],
+];
